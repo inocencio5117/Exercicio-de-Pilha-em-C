f80d2b43408551f0315b82b1ba679724708870a1
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-06-02 14:07:59 -0300

    Adding functions to pop and push itens in the stack

--- MAPA_vinicius_Inocencio_de_Souza.c
+++ MAPA_vinicius_Inocencio_de_Souza.c
@@ -2,61 +2,97 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _NODE{
-	int data;
-	struct NODE* next;
-} NODE;
+#define NUM_PLATES 5
 
-typedef struct _STACK{
-	NODE* top;
-} STACK;
+struct stack
+{
+   int data;
+   struct stack *ptr; //pointer type of stack
+};
 
-STACK *p;
+typedef struct stack Stack;
+typedef Stack *stackPtr;
 
-void print(NODE* ptr){
-	system("cls");
-	while(ptr != NULL){
-		printf("%d ", ptr->data);
-		ptr = ptr->next;
-	}
+void push( stackPtr *top, int x ) {
+    stackPtr nodePtr;
+
+    //allocating memory
+    nodePtr = malloc(sizeof(Stack));
+
+    //pushing value at the top of stack only if there is space
+    if(nodePtr != NULL) {
+       nodePtr -> data = x;
+       nodePtr -> ptr = *top;
+       *top = nodePtr;
+    }
+
+    else
+       printf("\nERROR !!! (Not enough space)");
 }
 
-void push(int element){
-	NODE* ptr = (NODE*) malloc(sizeof(NODE));
-	ptr->data = element;
-	ptr->next = p->top->next;
-	p->top = ptr;
+int pop(stackPtr *top)
+{
+   int pValue; //variable for value at the top
+   stackPtr tempPtr; //temporary pointer
+
+   tempPtr = *top;
+   pValue = (*top) -> data;
+   *top = (*top) -> ptr;
+   free(tempPtr); //free temporary pointer value
+   return pValue;
 }
 
-// int pop(){
-// 	NODE* ptr = p->top;
-// 	int data;
-// 	if(ptr != NULL){
-// 		p->top = ptr->next;
-// 		ptr->next = NULL;
-// 		data = ptr->data;
-// 		free(ptr);
-// 		return data;
-// 	}
-// }
+void printValue(stackPtr showPtr) {
+   if(showPtr == NULL) {
+       printf("\nNo value found.");
+   }
+   else {
+      printf("\nData stored in stack:\n");
 
-#define PLATES_NUMB 5
+      while(showPtr != NULL) {
+         printf("%d --> ", showPtr -> data);
+         showPtr = showPtr -> ptr;
+     }
+     printf("NULL\n");
+   }
+}
+
+int checkEmpty(stackPtr top)
+{
+   return top == NULL;
+}
+
 // pratos -> (1) Vermelho, (2) Verde, (3) Azul, (4) Branco e (5) Laranja
-char *plates[PLATES_NUMB] = {"Vermelho", "Verde", "Azul", "Branco", "Laranja"};
+char *arr3[NUM_PLATES] = { "Vermelho",
+                            "Verde",
+                            "Azul",
+                            "Branco",
+                            "Laranja" };
 
 int main() {
   // passo 1 -> Preencher uma pilha com os cinco pratos citados acima;
-  // for (int i = 0; i < PLATES_NUMB; ++i) {
-  //   printf("%s\n", plates[i]);
-  //   push(i);
-  // }
+  stackPtr stackNewPtr = NULL;
 
-  push(1);
-  print(p);
+  for (int i = 0; i < 5; ++i) {
+    push(&stackNewPtr, i);
+  }
 
   // passo 2 ->  Imprimir a pilha na ordem em que os elementos foram inseridos (iniciando pela base, e finalizando pelo top);
+  printValue(stackNewPtr);
 
   // passo 3 -> Desempilhar todos os elementos, inserindo-os em uma segunda estrutura de datas do tipo pilha.
+  stackPtr secondStackPtr = NULL;
 
+  for (int i = NUM_PLATES; i > 0; --i) {
+    if (!checkEmpty(stackNewPtr)) {
+      int popedValue = pop(&stackNewPtr);
+      printf("\nPopped Value: %d\n", popedValue);
+      push(&secondStackPtr, popedValue);
+    }
+  }
+  
   // passo 4 -> Imprimir a nova pilha na ordem em que os elementos foram inseridos (iniciando pela base, e finalizando pelo top).
+
+  printf("\nNew stack:\n");
+  printValue(secondStackPtr);
 }
